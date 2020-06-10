5dddf7ba270e2c2a9c6adb262544d2ee27f53b6f
arijitiiest <1999arijitchoudhury@gmail.com>
2020-06-10 16:16:37 +0530

    :heart:

---
+++ precache-manifest.bd1c741feee14791ce8bc740efeaa342.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2764a6809a6f79c9555cc7c0c9b8f31d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de6c3a9ce1a606236f13",
+    "url": "/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "965e343bd60267691f44",
+    "url": "/static/css/main.34003f78.chunk.css"
+  },
+  {
+    "revision": "de6c3a9ce1a606236f13",
+    "url": "/static/js/2.ae2b4381.chunk.js"
+  },
+  {
+    "revision": "965e343bd60267691f44",
+    "url": "/static/js/main.d5e41c9d.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "d267bb71b0f33e8e38196c1f0c2d808f",
+    "url": "/static/media/bengali_ocr.d267bb71.png"
+  },
+  {
+    "revision": "68499b31ec1461e66a6e5e49433ddbf9",
+    "url": "/static/media/bg.68499b31.jpg"
+  },
+  {
+    "revision": "73fe9007b3176e35251daa2797133063",
+    "url": "/static/media/biochip.73fe9007.png"
+  },
+  {
+    "revision": "bf28e185c8388af6f183971b45938f37",
+    "url": "/static/media/covid.bf28e185.png"
+  },
+  {
+    "revision": "af2a800c605d6a12c0c0bb6bdc4f426c",
+    "url": "/static/media/django.af2a800c.png"
+  },
+  {
+    "revision": "2103c44598b4a6454d3df6e57b3d1415",
+    "url": "/static/media/docker.2103c445.png"
+  },
+  {
+    "revision": "cb3f303abe8d32104d50bef2fd3931b7",
+    "url": "/static/media/express.cb3f303a.png"
+  },
+  {
+    "revision": "fcf31a787b7e15a9a692dd0d06554c80",
+    "url": "/static/media/face.fcf31a78.png"
+  },
+  {
+    "revision": "f0d21f6822198270524ffeea0562e722",
+    "url": "/static/media/flask.f0d21f68.png"
+  },
+  {
+    "revision": "e8b602700ebdac7dd369834a3275bc46",
+    "url": "/static/media/graphql.e8b60270.png"
+  },
+  {
+    "revision": "f9696b0c1d3832752f92f1999961a14e",
+    "url": "/static/media/hospital.f9696b0c.png"
+  },
+  {
+    "revision": "e3bd4b4d90b9d7bbd72db691b12c05a9",
+    "url": "/static/media/kth.e3bd4b4d.png"
+  },
+  {
+    "revision": "f551583d341e7ea9d860f660f9429bf3",
+    "url": "/static/media/malaria.f551583d.png"
+  },
+  {
+    "revision": "248448f7d1e838dc64fd2a89bf476157",
+    "url": "/static/media/mark.248448f7.png"
+  },
+  {
+    "revision": "f23a5524cf62c261b8ed761cbb70eae4",
+    "url": "/static/media/me.f23a5524.jpg"
+  },
+  {
+    "revision": "39fcf00aad489f95c6b5976c361de135",
+    "url": "/static/media/mysql.39fcf00a.png"
+  },
+  {
+    "revision": "e6da942ee14c5b9170962c0767739c6b",
+    "url": "/static/media/node.e6da942e.jpeg"
+  },
+  {
+    "revision": "0267a36faf53f0c12aa821fb7b221c90",
+    "url": "/static/media/opencv.0267a36f.png"
+  },
+  {
+    "revision": "d6f235109272052b5c0285ef92392f65",
+    "url": "/static/media/pathfinding.d6f23510.png"
+  },
+  {
+    "revision": "597cd41b2d741adf3eff7d5a1d695cc1",
+    "url": "/static/media/postgres.597cd41b.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/static/media/reactjs.37fe8322.png"
+  },
+  {
+    "revision": "1c7eb472bc64420ce6b877071ca13735",
+    "url": "/static/media/reactnative.1c7eb472.png"
+  },
+  {
+    "revision": "3df859caf9868891e5914d27f87426f4",
+    "url": "/static/media/sorting.3df859ca.png"
+  },
+  {
+    "revision": "0dfae3280e388283dc05ffc8620bf1e5",
+    "url": "/static/media/tensorflow.0dfae328.png"
+  },
+  {
+    "revision": "ba66984a2ae315c1f921ba4f685bd839",
+    "url": "/static/media/uci.ba66984a.png"
+  }
+]);
